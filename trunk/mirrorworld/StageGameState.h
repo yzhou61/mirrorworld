de775c638531f77b8fa522fb35b3f0e71e4b568b
cxyzs7 <cxyzs7@2e672104-186a-11df-85bb-b9a83fee2cb1>
2010-02-16 00:13:07 +0000

    Added StageGameState

--- trunk/mirrorworld/StageGameState.h
+++ trunk/mirrorworld/StageGameState.h
@@ -6,6 +6,35 @@
 #ifndef _STAGE_GAME_STATE_H_
 #define _STATE_GAME_STATE_H_
 
+#include "GameState.h"
 
+class StageGameState : public GameState
+{
+public:
+    static void create(GameStateManager* manager, const Ogre::String name);
+    void enter();
+    void createScene();
+    void exit();
+    bool pause();
+    void resume();
+
+    void update(double timeSinceLastFrame);
+
+    bool keyPressed(const OIS::KeyEvent &keyEventRef);
+    bool keyReleased(const OIS::KeyEvent &keyEventRef);
+
+    bool mouseMoved(const OIS::MouseEvent &evt);
+    bool mousePressed(const OIS::MouseEvent &evt, OIS::MouseButtonID id); 
+    bool mouseReleased(const OIS::MouseEvent &evt, OIS::MouseButtonID id);
+
+    void moveCamera();
+    void handleInput();
+
+private:
+    StageGameState(GameStateManager* manager) : GameState(manager) {}
+    bool            m_bQuit;
+    // Mouse State
+    bool            m_bRMouseDown, m_bLMouseDown;
+};
 
 #endif
