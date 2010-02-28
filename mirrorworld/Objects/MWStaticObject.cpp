8f5d6f42dd380d71187310d175f4c71de8eae124
cxyzs7 <cxyzs7@2e672104-186a-11df-85bb-b9a83fee2cb1>
2010-02-28 23:02:08 +0000

    Oops, always forgot something

---
+++ mirrorworld/Objects/MWStaticObject.cpp
@@ -0,0 +1,26 @@
+//////////////////////////////////////////////////////////////////////////
+//
+//////////////////////////////////////////////////////////////////////////
+#include "MWStaticObject.h"
+
+namespace MirrorWorld {
+
+void StaticObject::setEntity(Ogre::SceneManager *sceneMgr, OgreNewt::World *world, Ogre::SceneNode *node, Ogre::Entity *entity)
+{
+    m_Entity = entity; m_Entity->setUserAny(Ogre::Any(this));
+
+    OgreNewt::CollisionPtr colPtr(new OgreNewt::CollisionPrimitives::TreeCollision(world, entity, true, 0));
+    m_pPhyBody = new OgreNewt::Body(world, colPtr);
+
+#ifdef OGRENEWT_NO_COLLISION_SHAREDPTR
+    delete camCollision;
+#endif
+
+    m_pPhyBody->setPositionOrientation(node->getPosition(), Ogre::Quaternion::IDENTITY);
+    m_pPhyBody->attachNode(node);
+
+    if (m_pPhyMatID)
+        m_pPhyBody->setMaterialGroupID(m_pPhyMatID);
+}
+
+}
