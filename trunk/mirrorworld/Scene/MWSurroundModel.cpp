6b3e1d99d1a08b863c7119f8fe323ce4f9b8669d
cxyzs7 <cxyzs7@2e672104-186a-11df-85bb-b9a83fee2cb1>
2010-03-11 04:30:51 +0000

    oops

---
+++ trunk/mirrorworld/Scene/MWSurroundModel.cpp
@@ -0,0 +1,56 @@
+//////////////////////////////////////////////////////////////////////////
+//
+//////////////////////////////////////////////////////////////////////////
+#include "MWSurroundModel.h"
+
+int MirrorWorld::SurroundModel::m_nUniqueID = 0;
+
+namespace MirrorWorld
+{
+SurroundModel::SurroundModel(Ogre::SceneManager* sceneMgr)
+{
+    Ogre::String id = Ogre::StringConverter::toString(m_nUniqueID++);
+    m_pSceneMgr = sceneMgr;
+    m_pParticleSys = m_pSceneMgr->createParticleSystem("SurroundParticle"+id, "Surround");
+    m_pSceneNode = m_pSceneMgr->getRootSceneNode()->createChildSceneNode("SurroundParticleNode"+id);
+    m_bLive = false;
+}
+
+void SurroundModel::active(const Ogre::Vector3& pos, const Ogre::Vector3& normal, const Ogre::Vector3& up)
+{
+    if (m_bLive)
+        return;
+    Ogre::Quaternion q = Ogre::Vector3::UNIT_Z.getRotationTo(normal);
+    Ogre::Vector3 newUp = q * Ogre::Vector3::UNIT_Y;
+    m_pSceneNode->resetToInitialState();
+    m_pSceneNode->rotate(newUp.getRotationTo(up));
+    m_pSceneNode->rotate(q);
+    m_pSceneNode->setPosition(pos);
+    m_pParticleSys->clear();
+    for (int i = 0;i < 4; i++)
+    {
+        m_pParticleSys->getEmitter(i)->setEnabled(true);
+    }
+    Ogre::LogManager::getSingleton().stream()<<"haha"<<m_bLive;
+    m_pSceneNode->attachObject(m_pParticleSys);
+    Ogre::LogManager::getSingleton().stream()<<"haha2";
+    m_bLive = true;
+}
+
+void SurroundModel::deactive()
+{
+    Ogre::LogManager::getSingleton().logMessage("here");
+    m_pSceneNode->detachAllObjects();
+    m_bLive = false;
+}
+
+bool SurroundModel::update(double timeElasped)
+{
+    return m_bLive;
+}
+
+void SurroundModel::destroy()
+{
+
+}
+}
---
+++ trunk/mirrorworld/Scene/MWSurroundModel.h
@@ -0,0 +1,30 @@
+//////////////////////////////////////////////////////////////////////////
+//
+//////////////////////////////////////////////////////////////////////////
+#ifndef _MW_SURROUND_H_
+#define _MW_SURROUND_H_
+
+#include <Ogre.h>
+
+namespace MirrorWorld
+{
+class SurroundModel
+{
+public:
+    SurroundModel(Ogre::SceneManager* sceneMgr);
+    ~SurroundModel() { destroy(); }
+    void active(const Ogre::Vector3& pos, const Ogre::Vector3& normal, const Ogre::Vector3& up);
+    void deactive();
+    bool update(double timeElasped);
+    void destroy();
+    static void resetCounter() { m_nUniqueID = 0; }
+private:
+    bool                    m_bLive;
+    static int              m_nUniqueID;
+    Ogre::SceneManager*     m_pSceneMgr;
+    Ogre::SceneNode*        m_pSceneNode;
+    Ogre::ParticleSystem*   m_pParticleSys;
+};
+}
+
+#endif
